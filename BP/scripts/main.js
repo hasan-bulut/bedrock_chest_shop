f72cc9a31856abbf13e8aab4701845508bde4cf0
Hasan Bulut <buluthasan853@gmail.com>
2023-12-24 01:57:08 +0300

    plugin added

---
+++ BP/scripts/main.js
@@ -0,0 +1,34 @@
+import { world, system, ItemStack } from "@minecraft/server";
+import { ActionFormData, MessageFormData, ModalFormData } from "@minecraft/server-ui";
+
+function createForm(item) {
+    const form = new ModalFormData();
+    form.title("Pazar Oluştur");
+    form.textField(item.typeId + "\n\nMiktar", "64")
+    form.textField("Fiyat", "100")
+
+    return form;
+}
+
+world.afterEvents.playerInteractWithBlock.subscribe(event => {
+    var player = event.player;
+    var block = event.block;
+    var item = event.itemStack;
+
+    if (player.isSneaking &&
+        block.typeId == "minecraft:chest" &&
+        item.typeId == "minecraft:oak_sign") {
+
+        createForm(item).show(player).then(data => {
+            switch (data.selection) {
+                case 0:
+                    world.sendMessage('button 1')
+                    break;
+                case 1:
+                    world.sendMessage('button 2')
+                    break;
+            }
+        });
+
+    }
+})
